b20a61e6e34839333a4b25214cadfce66a795e22
Victoire Nyoung <nyoungvictoire@gmail.com>
2021-09-06 13:54:09 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from './components/Button'
+import Add from './components/Add'
+import List from './components/List'
+import Pay from './components/Pay'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      activeTab: "add",
+      items: []
+    }
+  }
+
+  handleButtonClick = (e) => {
+    this.setState({ activeTab: e.target.innerText })
+  }
+
+  addItem = (itemName, itemPrice) => {
+    this.setState({ items: [ ...this.state.items, { name: itemName, price: itemPrice } ] })
+  }
+
+	render() {
+    console.log(this.state)
+
+		return(
+      <>
+        <Button
+          isSelected={this.state.activeTab === "add"}
+          handleClick={this.handleButtonClick}
+        >
+            add
+        </Button>
+        <Button
+          isSelected={this.state.activeTab === "list"}
+          handleClick={this.handleButtonClick}
+        >
+          list
+        </Button>
+        <Button
+          isSelected={this.state.activeTab === "pay"}
+          handleClick={this.handleButtonClick}
+        >
+          pay
+        </Button>
+        
+        {this.state.activeTab === "add" && <Add addItem={this.addItem} />}
+        {this.state.activeTab === "list" && <List items={this.state.items} />}
+        {this.state.activeTab === "pay" && <Pay />}
+      </>
+		)
+	}
+}
+
+export default App
